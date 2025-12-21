408a2a153d74d91e307cfa9cf20723f54b40ef29
bachtiarfadli-gif <bachtiarfadli@gmail.com>
2025-12-22 01:25:25 +0700

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const visiblePoint = 120;
+
+    if (elementTop < windowHeight - visiblePoint) {
+      el.classList.add("active");
+    }
+  });
+});
